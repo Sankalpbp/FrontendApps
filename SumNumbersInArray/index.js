2fea0085c0e7a063ca583107c1fee78e15d9deda
Sankalpbp <sankalpnitj@gmail.com>
2023-07-26 03:20:55 +0530

    Added reset button

--- SumNumbersInArray/index.js
+++ SumNumbersInArray/index.js
@@ -1,12 +1,37 @@
 'use strict';
 
 const array = document.getElementsByTagName ( 'td' );
+const button = document.getElementById('get-sum-button');
+const sumElement = document.getElementById ( 'sum' );
+const tableContainer = document.querySelector('.table-container');
+const input = document.getElementsByTagName('input')[0];
 let index = 0;
 let sum = 0;
 
+function putInitState () {
+    button.disabled = false;
+    button.innerText = 'Sum Up!';
+    index = 0;
+    sum = 0;
+    sumElement.innerText = sum;
+    const existingTable = document.getElementsByTagName('table');
+    if (existingTable.length) {
+        tableContainer.removeChild(existingTable[0]);
+    } 
+}
+
+function reset () {
+    putInitState ();
+    input.value = '';
+}
+
 function createTable () {
-    const row = document.getElementsByTagName('tr')[0];
-    const input = document.getElementsByTagName('input')[0];
+    putInitState();
+    const table = document.createElement('table');
+    const row = document.createElement('row');
+    table.appendChild(row);
+    table.setAttribute('border', 1);
+    tableContainer.appendChild(table);
     const elements = input.value.split (',');
     for(let element of elements) {
         const td = document.createElement('td');
@@ -17,12 +42,13 @@ function createTable () {
 
 function getSum () {
     if (index == array.length - 1) {
-        const button = document.getElementById('get-sum-button');
         button.innerText = 'All elements added!';
         button.disabled = true;
     }
     array[index].classList.add('added');
     sum += (Number) (array [ index ].innerText);
     ++index;
-    document.getElementById ( 'sum' ).innerHTML = sum;
+    sumElement.innerHTML = sum;
 }
+
+putInitState ();
