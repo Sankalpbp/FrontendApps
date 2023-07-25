8c1cefc6fa5be8ae9adfc234fddabde08685b32d
Sankalpbp <sankalpnitj@gmail.com>
2023-07-26 02:58:15 +0530

    Created table dynamically using input from user

--- SumNumbersInArray/index.js
+++ SumNumbersInArray/index.js
@@ -4,9 +4,20 @@ const array = document.getElementsByTagName ( 'td' );
 let index = 0;
 let sum = 0;
 
+function createTable () {
+    const row = document.getElementsByTagName('tr')[0];
+    const input = document.getElementsByTagName('input')[0];
+    const elements = input.value.split (',');
+    for(let element of elements) {
+        const td = document.createElement('td');
+        td.innerText = element;
+        row.appendChild(td);
+    }
+}
+
 function getSum () {
     if (index == array.length - 1) {
-        const button = document.getElementsByTagName('button')[0];
+        const button = document.getElementById('get-sum-button');
         button.innerText = 'All elements added!';
         button.disabled = true;
     }
